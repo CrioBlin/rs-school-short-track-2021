67b248539c4a7d5f340d6f9b0688fea53015e00d
Oleksandr Kaminetskyi <crioblin11@gmail.com>
2021-04-09 08:48:26 +0300

    feat: implement solution for 6 task

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -12,8 +12,15 @@
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  // throw new Error('Not implemented');
+  const regEx = /[G-Z]/i;
+
+  if (n.match(regEx)) {
+    return false;
+  }
+
+  return true;
 }
 
 module.exports = isMAC48Address;
